26827d89da60477e15bf4b378baf0f252fccb516
Rodrigo Bonilha Tolledo <rodrigotolledo@gmail.com>
2018-09-09 10:54:35 +1000

    removed async

--- page-object-test/AngularHomepage.js
+++ page-object-test/AngularHomepage.js
@@ -2,15 +2,15 @@ var AngularHomePage = function() {
     var nameInput = element(by.model('yourName'));
     var greeting = element(by.binding('yourName'));
 
-    this.get = async function() {
+    this.get = function() {
         browser.get('https://www.angularjs.org');
     };
 
-    this.setName = async function(name) {
+    this.setName = function(name) {
         nameInput.sendKeys(name);
     };
 
-    this.getGreetingText = async function() {
+    this.getGreetingText = function() {
         return greeting.getText();
     };
 };
